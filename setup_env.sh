85e1399a7d6ffe813a4a578887d9fb3a4a3418dd
Nate Woythaler <nwoythaler@ddn.com>
2017-12-01 08:56:21 -0700

    Created basic virtualenv setup script

---
+++ setup_env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+virtualenv --help
+if[ $? ] then;
+    echo "Virtualenv not detected! Attempting install..."
+    sudo -H pip install virtualenv --no-cache  # Needs sudo password auth from user?
+fi
+
+# Create env, change to it
+virtualenv BeerBuddy/static/env
+. BeerBuddy/static/env/bin/activate
+
+for LINE in $(cat dependencies.txt); do
+    pip install $LINE --no-cache-dir
+done
